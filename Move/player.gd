f0596514a5f9ad11eaaf37cd144e41f787a0da7c
Jaytat00 <emz.doolan07@gmail.com>
2026-03-23 14:39:48 +0000

    DASH!!!!!
    I added dash with spacebar yippeee

--- Move/player.gd
+++ Move/player.gd
@@ -4,12 +4,17 @@ extends CharacterBody2D
 @export var speed : float = 150
 @export var animation_tree : AnimationTree
 
+
 var input : Vector2
 var playback : AnimationNodeStateMachinePlayback
 
+
+#animation running the last frame compared to new one
 func _ready():
 	playback = animation_tree["parameters/playback"]
 
+
+#movement (input, actual moving)
 func _physics_process(_delta: float) -> void:
 	input = Input.get_vector("left" , "right" , "up" , "down")
 	velocity = input * speed
@@ -17,9 +22,18 @@ func _physics_process(_delta: float) -> void:
 	select_animation()
 	update_animation_parameters()
 	
+	#Handles dash
+	if Input.is_action_just_pressed("Dash"):
+		$DashTimer.start()
+		speed *= 10
+		velocity = input * speed
+	#runs animation
+	
+	
 func select_animation(): 
 	if velocity == Vector2.ZERO:
 		playback.travel("walk")
+		
 	
 func update_animation_parameters(): 
 	if input == Vector2.ZERO:
@@ -27,3 +41,7 @@ func update_animation_parameters():
 		
 	animation_tree["parameters/walk/blend_position"] = input
 	
+
+ #reset dash
+func _on_dash_timer_timeout() -> void:
+	speed = 150
