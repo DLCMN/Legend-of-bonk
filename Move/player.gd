e979ace3c560f88b44846bcf292798ff7dca7a5a
Jaytat00 <emz.doolan07@gmail.com>
2026-03-29 23:46:10 +0100

    VISUAL ONE ATTACK !!1
    I added attack 1, works visually 95% of the time, (it works fine, code good) I just need to add combos and actual damage, then we flying. I do that tmr, just wanted something on git for our standup that works

--- Move/player.gd
+++ Move/player.gd
@@ -7,32 +7,53 @@ extends CharacterBody2D
 
 var input : Vector2
 var playback : AnimationNodeStateMachinePlayback
-
+var is_attacking = false
 
 #animation running the last frame compared to new one
-func _ready():
+func _ready() -> void:
 	playback = animation_tree["parameters/playback"]
 
 
 #movement (input, actual moving)
 func _physics_process(_delta: float) -> void:
-	input = Input.get_vector("left" , "right" , "up" , "down")
-	velocity = input * speed
-	move_and_slide()
-	select_animation()
-	update_animation_parameters()
+	input = Input.get_vector("left", "right", "up", "down")
 	
-	#Handles dash
-	if Input.is_action_just_pressed("Dash"):
+	if Input.is_action_just_pressed("Attack") and not is_attacking:
+		is_attacking = true
+		velocity = Vector2.ZERO
+		playback.travel("Attack1") #go to attack
+		
+	
+	else:
+		#only move when not attacking pls
+		if not is_attacking:
+			velocity = input * speed
+		else:
+			velocity = Vector2.ZERO
+
+		move_and_slide()
+		select_animation()
+		update_animation_parameters()
+
+	# dash
+	if Input.is_action_just_pressed("Dash") and not is_attacking:
 		$DashTimer.start()
 		speed *= 10
 		velocity = input * speed
+
 	#runs animation
+
+func attack_finished():
+	is_attacking = false
 	
-	
-func select_animation(): 
+func select_animation() -> void:
+	if is_attacking:
+		return
 	if velocity == Vector2.ZERO:
+		playback.travel("Idle")
+	else :
 		playback.travel("walk")
+		
 		
 	
 func update_animation_parameters(): 
@@ -40,6 +61,8 @@ func update_animation_parameters():
 		return
 		
 	animation_tree["parameters/walk/blend_position"] = input
+	animation_tree["parameters/Idle/blend_position"] = input
+	animation_tree["parameters/Attack1/blend_position"] = input
 	
 
  #reset dash
