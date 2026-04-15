505f44d117aac67a6d025d3ea404099c0db12c76
ERROR <daithicremin@gmail.com>
2026-04-15 14:43:27 +0100

    Revert "More changes"
    This reverts commit f98b7997b38144a87e42ae58d5552c6dd1b09f79, reversing
    changes made to 3dd9db2a3278d7a1b9847c3511c629df33a09c60.

--- newEnemies/EvilSleem/Evilsleem.gd
+++ newEnemies/EvilSleem/Evilsleem.gd
@@ -3,16 +3,11 @@ extends CharacterBody2D
 const SPEED: int = 100
 
 var knockbackForce: int = 150
-
-const DROP_CHANCE:float = 1.0
-
 var alive: bool = true
 var target = null
 var targetInRange:bool = false
 var strength: int  = 10
 var health: int = 75
-
-var health_pickup_scene=preload("res://health_pickup.tscn")
 
 @onready var animSprite: AnimatedSprite2D = $evilSleemSprite
 @onready var soundDamage: AudioStreamPlayer2D = $SleemHit
@@ -76,10 +71,6 @@ func die() -> void:
 	$Sight/CollisionShape2D.set_deferred("disabled", true)
 	$Hitbox/CollisionShape2D.set_deferred("disabled", true)
 
-#Drop health pickup
-	if randf()<=DROP_CHANCE:
-		drop_item()
-
 	#targeting the player
 func _on_sight_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
@@ -113,14 +104,3 @@ func _on_hitbox_body_exited(body: Node2D) -> void:
 func _on_attack_timer_timeout() -> void:
 	if target and targetInRange:
 		target.takeDamage(strength)
-
-
-func drop_item():
-	var drop = health_pickup_scene.instantiate()
-	drop.position=position 
-	var level_root = get_parent()
-	var items_node= level_root.get_node("Items")
-	
-	items_node.call_deferred("add_child", drop)
-	
-	
