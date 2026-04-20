3861e5dff4b657218748bced76b2adbd3bf0290a
Alex91R <alex.ridteygi@gmail.com>
2026-04-20 12:00:04 +0100

    Added UI

--- Script/key_item.gd
+++ Script/key_item.gd
@@ -1,45 +1,19 @@
-class_name Door extends Node2D
-#Door is a Node2D so it can be rotated easily
+extends Node2D
+# This script controls the key pickup object
 
-@export var locked_audio : AudioStream
-@export var open_audio : AudioStream 
+@export var key_id: String
+# Identifier for this key
+# Example values: "red_key", "blue_key", "boss_key"
 
-@onready var body = $StaticBody2D  # Reference to physics body that blocks the player
-@onready var animation: AnimationPlayer = $AnimationPlayer
-@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
-@onready var interact_area: Area2D = $InteractArea2D
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	# Automatically called when another body enters this Area2D
 
-@export var required_key_id: String = ""
-# Which key opens this door
-# Empty string = unlocked door
+	if body.has_node("KeyRing"):
+		# Check if the colliding body is the player
+		# The player must have a KeyRing node
 
-@export var consume_key := true
-# If true, key is removed after use
+		body.get_node("KeyRing").add_key(key_id)
+		# Give this key to the player's inventory
 
-var opened := false
-# Prevents door from opening multiple times
-
-
-func _on_Area2D_body_entered(player): # Called when player enters door's interaction area
-
-	if opened: # If already open, do nothing
-		return
-
-	if not player.has_node("KeyRing"): # Ensure this is the player
-		return
-
-	var keyring = player.get_node("KeyRing") # Access player's inventory
-	if required_key_id != "": # Door is locked
-
-		if not keyring.has_key(required_key_id): # Player does not have the key
-			locked_audio.play("closed") # Play the door opening animation
-			return
-
-		if consume_key: # Remove key after use (if enabled)
-			keyring.use_key(required_key_id)
-	open() # Open the door
-
-func open(): # Actually opens the door
-	opened = true # Mark as open
-	body.get_node("CollisionShape2D").disabled = true # Disable collision so player can pass
-	animation.play("open") # Play the door opening animation
+		queue_free()
+		# Remove the key from the level after pickup
